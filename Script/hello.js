16e2b7a9f664db8e0300af89ac7b10b29cb3c853
Andrey Kiselev <Kiseleq@gmail.com>
2021-01-30 11:44:01 +0300

    Выполнил задание 2

--- Script/hello.js
+++ Script/hello.js
@@ -1,9 +1,26 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+const money = 500;
+const income = 'Макдональдс';
+const addExpenses = 'Лифт, Радио, Интернет';
+const deposit = true;
+const mission = 6305;
+const period = 5;
 
-alert ("Привет!");
-console.log ("1, 2, 3");
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+console.log(income .length);
+
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
+
+console.log(addExpenses .toLowerCase());
+console.log(addExpenses.split(', '));
+
+let budgetDay;
+budgetDay = money / 30;
+console.log (budgetDay);
+
+
+
+
