e95863159c523fd33c041cd3e7299b6e45954a81
Andrey Kiselev <Kiseleq@gmail.com>
2021-01-30 16:28:41 +0300

    Изменил код в уроке 2 (2)

--- Script/hello.js
+++ Script/hello.js
@@ -9,17 +9,14 @@ console.log(typeof money);
 console.log(typeof income);
 console.log(typeof deposit);
 
-console.log(income .length);
+console.log(income.length);
 
 console.log('Период равен ' + period + ' месяцев');
 console.log('Цель заработать ' + mission + ' рублей');
 
-console.log(addExpenses .toLowerCase());
-console.log(addExpenses.split(', '));
+console.log(addExpenses.toLowerCase().split(', '));
 
-let budgetDay;
-budgetDay = money / 30;
-console.log (budgetDay);
+const budgetDay = money / 30;
 
 
 
